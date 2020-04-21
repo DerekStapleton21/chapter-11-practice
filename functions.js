b362e0b31be808c1e2a1bf4f517625e8dfddcd07
Derek Stapleton <derek.stapleton21@gmail.com>
2020-04-21 14:53:18 -0400

    chapter 11 practice

---
+++ functions.js
@@ -0,0 +1,11 @@
+function buildContactForm(){
+    return `
+    <form>
+       <h1>Name:</h1>
+       <input id="new-contact-name" type="text" placeholder="name" >
+       <h2>Message:</h2>
+       <input id="new-contact-msg"type="text" placeholder="message">
+       </form>
+       <button id="new-send-btn">Send</button>`
+}
+
